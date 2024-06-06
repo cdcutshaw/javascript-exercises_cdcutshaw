084c653c5835831fa802bd8d12c3631a3b5bc83d
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-06-06 14:16:14 -0600

    assignment completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+let repeatString = function(word, times)
+{
+    let string = word;
+    let i = 1;
 
-};
+    if(times == 0)
+        {
+            return "";
+        }
+    
+    if(times < 0)
+        {
+            return "ERROR";
+        }
+
+    while(times > i)
+        {
+            string = string + word;
+            i++;
+        }
+
+    return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
