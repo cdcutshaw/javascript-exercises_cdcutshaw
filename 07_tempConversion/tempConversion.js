0514d6940b105ac157e1691fe9518a6879ea79a4
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-06-07 17:03:36 -0600

    assignment completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFarenheit)
+{
+
+let degreesCelcius = ((degreesFarenheit - 32) * (5/9))
+
+let degreesCelciusRounded = Math.round(degreesCelcius * 10) /10
+
+return degreesCelciusRounded
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelcius) 
+{
+  
+let degreesFarenheit = ((degreesCelcius * (9/5)) + 32) 
+
+let degreesFarenheitRounded = Math.round(degreesFarenheit * 10) /10
+
+return degreesFarenheitRounded
+
 };
 
 // Do not edit below this line
