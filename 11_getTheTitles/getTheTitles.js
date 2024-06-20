ce5863d79c9d07f8c8e0f8323f9957f6dcf68a88
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-06-20 13:07:16 -0600

    assignment completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = array.map (books => books.title);
+    return titles
 };
 
 // Do not edit below this line
